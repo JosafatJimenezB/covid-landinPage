d218582cd1d19a896d0a8db519f886be6e297290
josafat <josa221001@gmail.com>
2023-03-07 22:21:50 -0600

    add: diseño de la landing

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 
 toggle.addEventListener("click", () => {
-  menu.classList.toggle("toggle");
+    if(menu.classList.contains("toggle")){
+        menu.classList.remove("toggle");
+    }else{
+        menu.classList.add("toggle");
+    }
 });
